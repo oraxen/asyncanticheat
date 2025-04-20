0809a6912d1689fec6cd295de4d2374354855567
Thomas Marchand <thomas.marchand@tuta.io>
2025-04-20 18:02:49 +0100

    Remove minimize() from Velocity shadowJar
    Fixes runtime ClassNotFoundException on Velocity due to PacketEvents
    relying on reflection/service-loading that minimize() strips.

--- asyncanticheat-velocity/build.gradle.kts
+++ asyncanticheat-velocity/build.gradle.kts
@@ -3,7 +3,7 @@
 tasks.shadowJar {
     relocate("com.github.retrooper.packetevents", "md.thomas.asyncanticheat.velocity.shaded.packetevents.api")
     relocate("io.github.retrooper.packetevents", "md.thomas.asyncanticheat.velocity.shaded.packetevents.impl")
-    minimize()
+    // Don't use minimize() - it strips classes needed by PacketEvents reflection/service-loading
 }
 
 
